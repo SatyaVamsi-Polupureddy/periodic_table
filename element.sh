1093655dabb600d4f8cf538c24d49be3c86c70c6
SatyaVamsi Polupureddy <p.s.vamsi2021@gmail.com>
2025-06-09 05:49:28 +0000

    feat: commit
    git push https://SatyaVamsi-Polupureddy:github_pat_11BLRCKKY0tnpPTuORny
    kQ_vSWeldVKWcmk3YtIbqGHZrdg3ikhnqegCvDrbOOgZl42QDIQQCG02bXGDiK@github.com/SatyaVamsi-Polupureddy/periodic_table.git

--- element.sh
+++ element.sh
@@ -2,7 +2,6 @@
 
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
-# No argument
 if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
@@ -23,5 +22,19 @@ then
   echo "I could not find that element in the database."
 else
   IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELT BOIL <<< "$ELEMENT_QUERY"
+# Check if argument is atomic number
+if [[ $1 =~ ^[0-9]+$ ]]
+then
+  ELEMENT=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$1")
+else
+  ELEMENT=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE symbol='$1' OR name='$1'")
+fi
+
+# If element not found
+if [[ -z $ELEMENT ]]
+then
+  echo "I could not find that element in the database."
+else
+  IFS="|" read -r ATOMIC_NUMBER SYMBOL NAME MASS MELT BOIL TYPE <<< "$ELEMENT"
   echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
 fi
