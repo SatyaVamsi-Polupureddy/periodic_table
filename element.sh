2cc6e6ca8024800e96f98e7e46864cb79cd5ca52
SatyaVamsi Polupureddy <p.s.vamsi2021@gmail.com>
2025-06-09 06:03:38 +0000

    fix: fix typo in element.sh

--- element.sh
+++ element.sh
@@ -2,39 +2,37 @@
 
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
-if [[ -z $1 ]]
-then
+# No argument given
+if [[ -z $1 ]]; then
   echo "Please provide an element as an argument."
   exit
 fi
 
-# Check type of input
-if [[ $1 =~ ^[0-9]+$ ]]
-then
-  ELEMENT_QUERY=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties USING(atomic_number) JOIN types USING(type_id) WHERE atomic_number=$1")
+# Determine the query based on input format
+if [[ $1 =~ ^[0-9]+$ ]]; then
+  # argument is atomic number
+  ELEMENT=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, type 
+    FROM elements 
+    INNER JOIN properties USING(atomic_number) 
+    INNER JOIN types USING(type_id) 
+    WHERE atomic_number = $1")
 else
-  ELEMENT_QUERY=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties USING(atomic_number) JOIN types USING(type_id) WHERE symbol='$1' OR name='$1'")
+  # argument is symbol or name
+  ELEMENT=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, type 
+    FROM elements 
+    INNER JOIN properties USING(atomic_number) 
+    INNER JOIN types USING(type_id) 
+    WHERE symbol = INITCAP('$1') OR name = INITCAP('$1')")
 fi
 
-# Check if found
-if [[ -z $ELEMENT_QUERY ]]
-then
+# Not found
+if [[ -z $ELEMENT ]]; then
   echo "I could not find that element in the database."
-else
-  IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELT BOIL <<< "$ELEMENT_QUERY"
-# Check if argument is atomic number
-if [[ $1 =~ ^[0-9]+$ ]]
-then
-  ELEMENT=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$1")
-else
-  ELEMENT=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE symbol='$1' OR name='$1'")
+  exit
 fi
 
-# If element not found
-if [[ -z $ELEMENT ]]
-then
-  echo "I could not find that element in the database."
-else
-  IFS="|" read -r ATOMIC_NUMBER SYMBOL NAME MASS MELT BOIL TYPE <<< "$ELEMENT"
-  echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
-fi
+# Parse output
+IFS="|" read -r ATOMIC_NUMBER SYMBOL NAME MASS MELT BOIL TYPE <<< "$ELEMENT"
+
+# Final output
+echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
